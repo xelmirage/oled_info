f8d4acfa703ec792945d66f689c35a026c1f868f
xelmirage at xelmirage-pc <xelmirage@pku.edu.cn>
2015-10-07 19:30:20 +0800

    start

---
+++ README.md
@@ -0,0 +1,7 @@
+Inspired by hallad¡®s Ardupi_OLED  https://github.com/hallard/ArduiPi_OLED
+
+For Raspberry Pi 2
+
+display time and ip addresses through I2C 12864 OLED.
+
+for using hallad¡®s Ardupi_OLED, please visit http://hallard.me/adafruit-oled-display-driver-for-pi/
---
+++ oled_run.cpp
@@ -0,0 +1,515 @@
+/*********************************************************************
+This is an example for our Monochrome OLEDs based on SSD1306 drivers
+
+  Pick one up today in the adafruit shop!
+  ------> http://www.adafruit.com/category/63_98
+
+This example is for a 128x32|64 size display using SPI or I2C to communicate
+4 or 5 pins are required to interface
+
+Adafruit invests time and resources providing this open source code, 
+please support Adafruit and open-source hardware by purchasing 
+products from Adafruit!
+
+Written by Limor Fried/Ladyada  for Adafruit Industries.  
+BSD license, check license.txt for more information
+All text above, and the splash screen must be included in any redistribution
+
+02/18/2013 	Charles-Henri Hallard (http://hallard.me)
+						Modified for compiling and use on Raspberry ArduiPi Board
+						LCD size and connection are now passed as arguments on 
+						the command line (no more #define on compilation needed)
+						ArduiPi project documentation http://hallard.me/arduipi
+
+						
+*********************************************************************/
+
+#include "ArduiPi_OLED_lib.h"
+#include "Adafruit_GFX.h"
+#include "ArduiPi_OLED.h"
+#include <stdio.h>      
+#include <sys/types.h>
+#include <ifaddrs.h>
+#include <netinet/in.h> 
+#include <string.h> 
+#include <arpa/inet.h>
+#include <getopt.h>
+#include<time.h>
+#define PRG_NAME        "oled_demo"
+#define PRG_VERSION     "1.1"
+
+// Instantiate the display
+ArduiPi_OLED display;
+
+
+// Config Option
+struct s_opts
+{
+	int oled;
+	int verbose;
+} ;
+
+int sleep_divisor = 1 ;
+	
+// default options values
+s_opts opts = {
+	OLED_ADAFRUIT_SPI_128x32,	// Default oled
+  false										// Not verbose
+};
+
+#define NUMFLAKES 10
+#define XPOS 0
+#define YPOS 1
+#define DELTAY 2
+
+#define LOGO16_GLCD_HEIGHT 16 
+#define LOGO16_GLCD_WIDTH  16 
+static unsigned char logo16_glcd_bmp[] =
+{ 0b00000000, 0b11000000,
+  0b00000001, 0b11000000,
+  0b00000001, 0b11000000,
+  0b00000011, 0b11100000,
+  0b11110011, 0b11100000,
+  0b11111110, 0b11111000,
+  0b01111110, 0b11111111,
+  0b00110011, 0b10011111,
+  0b00011111, 0b11111100,
+  0b00001101, 0b01110000,
+  0b00011011, 0b10100000,
+  0b00111111, 0b11100000,
+  0b00111111, 0b11110000,
+  0b01111100, 0b11110000,
+  0b01110000, 0b01110000,
+  0b00000000, 0b00110000 };
+
+
+void testdrawbitmap(const uint8_t *bitmap, uint8_t w, uint8_t h) {
+  uint8_t icons[NUMFLAKES][3];
+  srandom(666);     // whatever seed
+ 
+  // initialize
+  for (uint8_t f=0; f< NUMFLAKES; f++) {
+    icons[f][XPOS] = random() % display.width();
+    icons[f][YPOS] = 0;
+    icons[f][DELTAY] = random() % 5 + 1;
+    
+    printf("x: %d", icons[f][XPOS]);
+    printf("y: %d", icons[f][YPOS]);
+    printf("dy: %d\n", icons[f][DELTAY]);
+  }
+
+  while (1) {
+    // draw each icon
+    for (uint8_t f=0; f< NUMFLAKES; f++) {
+      display.drawBitmap(icons[f][XPOS], icons[f][YPOS], logo16_glcd_bmp, w, h, WHITE);
+    }
+    display.display();
+    usleep(100000/sleep_divisor);
+    
+    // then erase it + move it
+    for (uint8_t f=0; f< NUMFLAKES; f++) {
+      display.drawBitmap(icons[f][XPOS], icons[f][YPOS],  logo16_glcd_bmp, w, h, BLACK);
+      // move it
+      icons[f][YPOS] += icons[f][DELTAY];
+      // if its gone, reinit
+      if (icons[f][YPOS] > display.height()) {
+	icons[f][XPOS] = random() % display.width();
+	icons[f][YPOS] = 0;
+	icons[f][DELTAY] = random() % 5 + 1;
+      }
+    }
+   }
+}
+
+
+void testdrawchar(void) {
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+
+  for (uint8_t i=0; i < 168; i++) {
+    if (i == '\n') continue;
+    display.write(i);
+    if ((i > 0) && (i % 21 == 0))
+      display.print("\n");
+  }    
+  display.display();
+}
+
+void testdrawcircle(void) {
+  for (int16_t i=0; i<display.height(); i+=2) {
+    display.drawCircle(display.width()/2, display.height()/2, i, WHITE);
+    display.display();
+  }
+}
+
+void testfillrect(void) {
+  uint8_t color = 1;
+  for (int16_t i=0; i<display.height()/2; i+=3) {
+    // alternate colors
+    display.fillRect(i, i, display.width()-i*2, display.height()-i*2, color%2);
+    display.display();
+    color++;
+  }
+}
+
+void testdrawtriangle(void) {
+  for (int16_t i=0; i<min(display.width(),display.height())/2; i+=5) {
+    display.drawTriangle(display.width()/2, display.height()/2-i,
+                     display.width()/2-i, display.height()/2+i,
+                     display.width()/2+i, display.height()/2+i, WHITE);
+    display.display();
+  }
+}
+
+void testfilltriangle(void) {
+  uint8_t color = WHITE;
+  for (int16_t i=min(display.width(),display.height())/2; i>0; i-=5) {
+    display.fillTriangle(display.width()/2, display.height()/2-i,
+                     display.width()/2-i, display.height()/2+i,
+                     display.width()/2+i, display.height()/2+i, WHITE);
+    if (color == WHITE) color = BLACK;
+    else color = WHITE;
+    display.display();
+  }
+}
+
+void testdrawroundrect(void) {
+  for (int16_t i=0; i<display.height()/2-2; i+=2) {
+    display.drawRoundRect(i, i, display.width()-2*i, display.height()-2*i, display.height()/4, WHITE);
+    display.display();
+  }
+}
+
+void testfillroundrect(void) {
+  uint8_t color = WHITE;
+  for (int16_t i=0; i<display.height()/2-2; i+=2) {
+    display.fillRoundRect(i, i, display.width()-2*i, display.height()-2*i, display.height()/4, color);
+    if (color == WHITE) color = BLACK;
+    else color = WHITE;
+    display.display();
+  }
+}
+   
+void testdrawrect(void) {
+  for (int16_t i=0; i<display.height()/2; i+=2) {
+    display.drawRect(i, i, display.width()-2*i, display.height()-2*i, WHITE);
+    display.display();
+  }
+}
+
+void testdrawline() {  
+  for (int16_t i=0; i<display.width(); i+=4) {
+    display.drawLine(0, 0, i, display.height()-1, WHITE);
+    display.display();
+  }
+  for (int16_t i=0; i<display.height(); i+=4) {
+    display.drawLine(0, 0, display.width()-1, i, WHITE);
+    display.display();
+  }
+  usleep(250000/sleep_divisor);
+  
+  display.clearDisplay();
+  for (int16_t i=0; i<display.width(); i+=4) {
+    display.drawLine(0, display.height()-1, i, 0, WHITE);
+    display.display();
+  }
+  for (int16_t i=display.height()-1; i>=0; i-=4) {
+    display.drawLine(0, display.height()-1, display.width()-1, i, WHITE);
+    display.display();
+  }
+  usleep(250000/sleep_divisor);
+  
+  display.clearDisplay();
+  for (int16_t i=display.width()-1; i>=0; i-=4) {
+    display.drawLine(display.width()-1, display.height()-1, i, 0, WHITE);
+    display.display();
+  }
+  for (int16_t i=display.height()-1; i>=0; i-=4) {
+    display.drawLine(display.width()-1, display.height()-1, 0, i, WHITE);
+    display.display();
+  }
+  usleep(250000/sleep_divisor);
+
+  display.clearDisplay();
+  for (int16_t i=0; i<display.height(); i+=4) {
+    display.drawLine(display.width()-1, 0, 0, i, WHITE);
+    display.display();
+  }
+  for (int16_t i=0; i<display.width(); i+=4) {
+    display.drawLine(display.width()-1, 0, i, display.height()-1, WHITE); 
+    display.display();
+  }
+  usleep(250000/sleep_divisor);
+}
+
+void testscrolltext(void) {
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(10,0);
+  display.clearDisplay();
+  
+  if (opts.oled == OLED_SH1106_I2C_128x64)
+    display.print("No scroll\non SH1106");
+  else
+    display.print("scroll");
+  display.display();
+ 
+  display.startscrollright(0x00, 0x0F);
+  sleep(2);
+  display.stopscroll();
+  sleep(1);
+  display.startscrollleft(0x00, 0x0F);
+  sleep(2);
+  display.stopscroll();
+  sleep(1);    
+  display.startscrolldiagright(0x00, 0x07);
+  sleep(2);
+  display.startscrolldiagleft(0x00, 0x07);
+  sleep(2);
+  display.stopscroll();
+}
+
+
+/* ======================================================================
+Function: usage
+Purpose : display usage
+Input 	: program name
+Output	: -
+Comments: 
+====================================================================== */
+void usage( char * name)
+{
+	printf("%s\n", name );
+	printf("Usage is: %s --oled type [options]\n", name);
+	printf("  --<o>led type\nOLED type are:\n");
+	for (int i=0; i<OLED_LAST_OLED;i++)
+		printf("  %1d %s\n", i, oled_type_str[i]);
+	
+	printf("Options are:\n");
+	printf("  --<v>erbose  : speak more to user\n");
+	printf("  --<h>elp\n");
+	printf("<?> indicates the equivalent short option.\n");
+	printf("Short options are prefixed by \"-\" instead of by \"--\".\n");
+	printf("Example :\n");
+	printf( "%s -o 1 use a %s OLED\n\n", name, oled_type_str[1]);
+	printf( "%s -o 4 -v use a %s OLED being verbose\n", name, oled_type_str[4]);
+}
+
+
+/* ======================================================================
+Function: parse_args
+Purpose : parse argument passed to the program
+Input 	: -
+Output	: -
+Comments: 
+====================================================================== */
+void parse_args(int argc, char *argv[])
+{
+	static struct option longOptions[] =
+	{
+		{"oled"	  , required_argument,0, 'o'},
+		{"verbose", no_argument,	  	0, 'v'},
+		{"help"		, no_argument, 			0, 'h'},
+		{0, 0, 0, 0}
+	};
+
+	int optionIndex = 0;
+	int c;
+
+	while (1) 
+	{
+		/* no default error messages printed. */
+		opterr = 0;
+
+    c = getopt_long(argc, argv, "vho:", longOptions, &optionIndex);
+
+		if (c < 0)
+			break;
+
+		switch (c) 
+		{
+			case 'v': opts.verbose = true	;	break;
+
+			case 'o':
+				opts.oled = (int) atoi(optarg);
+				
+				if (opts.oled < 0 || opts.oled >= OLED_LAST_OLED )
+				{
+						fprintf(stderr, "--oled %d ignored must be 0 to %d.\n", opts.oled, OLED_LAST_OLED-1);
+						fprintf(stderr, "--oled set to 0 now\n");
+						opts.oled = 0;
+				}
+			break;
+
+			case 'h':
+				usage(argv[0]);
+				exit(EXIT_SUCCESS);
+			break;
+			
+			case '?':
+			default:
+				fprintf(stderr, "Unrecognized option.\n");
+				fprintf(stderr, "Run with '--help'.\n");
+				exit(EXIT_FAILURE);
+		}
+	} /* while */
+
+	if (opts.verbose)
+	{
+		printf("%s v%s\n", PRG_NAME, PRG_VERSION);
+		printf("-- OLED params -- \n");
+		printf("Oled is    : %s\n", oled_type_str[opts.oled]);
+		printf("-- Other Stuff -- \n");
+		printf("verbose is : %s\n", opts.verbose? "yes" : "no");
+		printf("\n");
+	}	
+}
+/*======================================================================
+Function:getCardNum
+Purpose:Get net card number
+Input:-
+Output:-
+Comments:
+========================================================================*/
+int getNetCardCount()
+{
+    int nCount = 0;
+    FILE *file = fopen("/proc/net/dev", "r");
+    if(!file)
+    {   
+        printf("Open /proc/net/dev failed !\n");
+        return nCount;
+    }   
+
+    char szLine[512];
+    fgets(szLine, sizeof(szLine), file);
+    fgets(szLine, sizeof(szLine), file);
+
+    while(fgets(szLine, sizeof(szLine), file))
+    {   
+        char szName[128] = {0};
+        sscanf(szLine, "%s", szName);
+        int nLen = strlen(szName);
+        if(nLen <= 0)
+            continue;
+        if(szName[nLen - 1] == ':')
+            szName[nLen - 1] = 0;
+        if(strcmp(szName, "lo") == 0)
+            continue;
+        nCount++;
+    }
+    fclose(file);
+    file = NULL;
+    return nCount;
+}
+
+/* ======================================================================
+Function: main
+Purpose : Main entry Point
+Input 	: -
+Output	: -
+Comments: 
+====================================================================== */
+int main(int argc, char **argv)
+{
+	
+	struct ifaddrs * ifAddrStruct=NULL;
+    	struct ifaddrs * ifa=NULL;
+    	void * tmpAddrPtr=NULL;
+	int numNetCard;
+	numNetCard=getNetCardCount();
+	// Oled supported display in ArduiPi_SSD1306.h
+	// Get OLED type
+
+	time_t t = time(NULL);
+	struct tm tm = *localtime(&t);
+
+
+
+	parse_args(argc, argv);
+
+
+	// SPI
+	if (display.oled_is_spi_proto(opts.oled))
+	{
+		// SPI change parameters to fit to your LCD
+		if ( !display.init(OLED_SPI_DC,OLED_SPI_RESET,OLED_SPI_CS, opts.oled) )
+			exit(EXIT_FAILURE);
+	}
+	else
+	{
+		// I2C change parameters to fit to your LCD
+		if ( !display.init(OLED_I2C_RESET,opts.oled) )
+			exit(EXIT_FAILURE);
+	}
+
+	display.begin();
+	
+  // init done
+  display.clearDisplay();   // clears the screen  buffer
+  display.display();   		// display it (clear display)
+
+	if (opts.oled == 5)
+	{
+		// showing on this display is very slow (the driver need to be optimized)
+		sleep_divisor = 4;
+
+		for(char i=0; i < 12 ; i++)
+		{
+			display.setSeedTextXY(i,0);  //set Cursor to ith line, 0th column
+			display.setGrayLevel(i); //Set Grayscale level. Any number between 0 - 15.
+			display.putSeedString("Hello World"); //Print Hello World
+		}
+		
+		sleep(2);
+
+	}
+
+  display.clearDisplay();
+  getifaddrs(&ifAddrStruct);
+
+	// text display tests
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.setTextSize(1);
+  for (ifa = ifAddrStruct; ifa != NULL; ifa = ifa->ifa_next) {
+        if (!ifa->ifa_addr) {
+            continue;
+        }
+        if (ifa->ifa_addr->sa_family == AF_INET) { // check it is IP4
+            // is a valid IP4 Address
+            tmpAddrPtr=&((struct sockaddr_in *)ifa->ifa_addr)->sin_addr;
+            char addressBuffer[INET_ADDRSTRLEN];
+            inet_ntop(AF_INET, tmpAddrPtr, addressBuffer, INET_ADDRSTRLEN);
+            display.printf("%s:%s\n", ifa->ifa_name, addressBuffer); 
+        }/* else if (ifa->ifa_addr->sa_family == AF_INET6) { // check it is IP6
+            // is a valid IP6 Address
+            tmpAddrPtr=&((struct sockaddr_in6 *)ifa->ifa_addr)->sin6_addr;
+            char addressBuffer[INET6_ADDRSTRLEN];
+            inet_ntop(AF_INET6, tmpAddrPtr, addressBuffer, INET6_ADDRSTRLEN);
+            printf("%s IP Address %s\n", ifa->ifa_name, addressBuffer); 
+        }*/
+        
+    }
+    if (ifAddrStruct!=NULL) freeifaddrs(ifAddrStruct);
+
+
+
+
+
+
+
+ // display.printf("%d cards\n",numNetCard);
+ // display.setTextColor(BLACK, WHITE); // 'inverted' text
+  display.display();
+
+
+
+	// Free PI GPIO ports
+	display.close();
+
+}
+
+
